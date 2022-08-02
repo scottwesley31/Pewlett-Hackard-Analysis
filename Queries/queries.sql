9321fe5b6df803aef7e5a70034d88506de25b4ab
unknown <scottwesleybean@gmail.com>
2022-08-01 18:04:33 -0600

    Added retirement_info.csv

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -42,7 +42,5 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-DROP TABLE retirement_info;
-
 -- Display retirement_info table
 SELECT * FROM retirement_info;
