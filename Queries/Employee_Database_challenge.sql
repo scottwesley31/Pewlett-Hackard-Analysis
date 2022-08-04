cbbd109e617cfce2752181cf8392fe2c4113cf0c
unknown <scottwesleybean@gmail.com>
2022-08-03 20:05:57 -0600

    Completed Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -12,7 +12,7 @@ ON e.emp_no = ti.emp_no
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
-SELECT * FROM retirement_titles;
+-- SELECT * FROM retirement_titles;
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
@@ -24,4 +24,33 @@ FROM retirement_titles AS rt
 WHERE (rt.to_date = '9999-01-01')
 ORDER BY rt.emp_no, rt.to_date DESC;
 
-SELECT * FROM unique_titles;
+-- SELECT * FROM unique_titles;
+
+-- Create the retirement_titles table
+SELECT COUNT(title), title
+	INTO retiring_titles
+	FROM unique_titles
+	GROUP BY title
+	ORDER BY count DESC;
+	
+-- SELECT * FROM retiring_titles;
+
+-- Create the mentorship_eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON e.emp_no = de.emp_no
+	INNER JOIN titles AS ti
+		ON e.emp_no = ti.emp_no
+	WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	ORDER BY e.emp_no;
+
+-- SELECT * FROM mentorship_eligibility;
